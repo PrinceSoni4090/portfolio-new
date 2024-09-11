dbaccb319d66487ed369614e842feeee019be74f
PrinceSoni4090 <prince4090@gmail.com>
2024-09-11 12:15:36 +0530

    lc

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange>
         <Navbar/>
