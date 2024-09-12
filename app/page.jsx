8f92bb900d1f9a256c79e2622954e410e1edefcd
PrinceSoni4090 <prince4090@gmail.com>
2024-09-12 10:03:10 +0530

    c3

--- app/page.jsx
+++ app/page.jsx
@@ -33,15 +33,15 @@ export default function Home() {
   //   };
   // }, []);
 
-const words = ['Fullstack Developer', 'Frontend Developer', 'Backend Developer', 'Software Engineer', 'Fullstack Developer']
+  const words = ['Fullstack Developer', 'Frontend Developer', 'Backend Developer', 'Software Engineer', 'Fullstack Developer']
 
-const para = `skilled in Next JS, React JS, typeScript. My expertise includes building responsive, high-quality web applications and managing databases. I have a strong foundation in deploying live projects, writing clean code, and following best practices. Currently, I&apos;m seeking new opportunities to contribute to a dynamic team and deliver outstanding web solutions.`;
+  const para = `skilled in Next JS, React JS, typeScript. My expertise includes building responsive, high-quality web applications and managing databases. I have a strong foundation in deploying live projects, writing clean code, and following best practices. Currently, I&apos;m seeking new opportunities to contribute to a dynamic team and deliver outstanding web solutions.`;
   return (
     <main>
 
       {/*  About */}
       <section id="about" className="container px-4 py-12 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center md:px-16 md:py-20">
-      
+
        <div className="w-full mt-4 lg:mt-0 lg:w-1/2 lg:order-2">
   <Image
     src="/HeroImage1.jpg"
@@ -51,18 +51,18 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
     className="w-full h-full max-w-xs mx-auto border-2 border-purple-500 rounded-full shadow-lg transition-transform duration-300 ease-in-out hover:shadow-2xl hover:scale-105"
     style={{ borderRadius: '50%', boxShadow: '0 0 10px 4px rgba(128, 0, 128, 0.5)' }}
   />
+  
 </div>
-
         <div className="w-full text-center lg:text-left lg:w-1/2 lg:order-1 lg:-mt-8 mt-8 lg:mt-0">
           <h1 className=" text-4xl md:text-4xl lg:text-6xl font-semibold relative z-20bg-clip-text text-transparent bg-gradient-to-b font-bold text-3xl leading-snug  md:text-4xl">
-           <Cover>Prince Soni </Cover> 
+            <Cover>Prince Soni </Cover>
           </h1>
           <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
             {/* I'm a <span ref={el} /> */}
-           <span>I&apos;m a</span> <FlipWords words= {words} />
+            <span>I&apos;m a</span> <FlipWords words={words} />
           </h1>
           <div className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-          <TextGenerateEffect words={para} />
+            <TextGenerateEffect words={para} />
           </div>
           <div className="flex justify-center lg:justify-start">
             <Button className="mt-6 px-16 py-7 rounded-2xl bg-gradient-to-r from-purple-400 to-purple-600 hover:from-purple-500 hover:to-purple-700 transition-all duration-300 ease-in-out transform hover:scale-105 flex items-center">
@@ -73,12 +73,7 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
           </div>
         </div>
 
-      
-      </section>
-      
-      {/* SKILLS  */}
-      <section id="skills">  
-        <Skills />
+
       </section>
 
       {/* Projects  */}
@@ -90,9 +85,9 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
           </div>
           <div className="flex flex-wrap justify-center">
 
-            
-              
-                
+
+
+
             {/* <!-- Project 1 --> */}
             <div className="w-full max-w-sm mx-4 my-4 rounded-xl shadow-lg dark:bg-gray-900 transform transition-transform duration-500 hover:scale-105 hover:shadow-xl">
               <div className="p-6">
@@ -126,8 +121,8 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
                 </div>
               </div>
             </div>
-            
-           
+
+
             {/* <!-- Project 2 --> */}
             <div className="w-full max-w-sm mx-4 my-4  dark:bg-gray-900 rounded-lg shadow-2xl dar9:bg-gray-800 transform transition-transform duration-500 hover:scale-105 hover:shadow-3xl">
               <div className="p-6">
@@ -160,8 +155,8 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
                 </div>
               </div>
             </div>
-            
-             
+
+
             {/* <!-- Project 3 --> */}
             <div className="w-full max-w-sm mx-4 my-4  dark:bg-gray-900 rounded-lg shadow-2xl dar9:bg-gray-800 transform transition-transform duration-500 hover:scale-105 hover:shadow-3xl">
               <div className="p-6">
@@ -193,8 +188,8 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
                 </div>
               </div>
             </div>
-             
-             
+
+
             <div className="w-full max-w-sm mx-4 my-4  rounded-lg shadow-2xl dark:bg-gray-900 transform transition-transform duration-500 hover:scale-105 hover:shadow-3xl">
               <div className="p-6">
                 <div className="flex items-center justify-center mb-4">
@@ -222,8 +217,8 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
                 </div>
               </div>
             </div>
-            
-             
+
+
             <div className="w-full max-w-sm mx-4 my-4  rounded-lg shadow-2xl dark:bg-gray-900 transform transition-transform duration-500 hover:scale-105 hover:shadow-3xl">
               <div className="p-6">
                 <div className="flex items-center justify-center mb-4">
@@ -251,14 +246,20 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
                   </Button>
                 </div>
               </div>
-            </div>  
-              
-        
-            
+            </div>
+
+
+
           </div>
         </div>
 
       </section>
+
+      {/* SKILLS  */}
+      <section id="skills">
+        <Skills />
+      </section>
+
 
       {/* Education  */}
       <section id="education">
@@ -267,93 +268,94 @@ const para = `skilled in Next JS, React JS, typeScript. My expertise includes bu
 
       {/* Footer  */}
       <section>
-        <footer className="footer footer-center rounded p-10">
-          <nav className="grid grid-flow-col gap-4">
-            <footer>
-              <nav className=" font-semibold  flex justify-center space-x-4">
-                <ScrollLink
-                  to="about"
-                  smooth={true}
-                  duration={50}
-                  className="hover:underline cursor-pointer"
-                >
-                  About
-                </ScrollLink>
-                <ScrollLink
-                  to="skills"
-                  smooth={true}
-                  duration={50}
-                  className="hover:underline cursor-pointer"
-                >
-                  Skills
-                </ScrollLink>
-                <ScrollLink
-                  to="projects"
-                  smooth={true}
-                  duration={50}
-                  className="hover:underline cursor-pointer"
-                >
-                  Projects
-                </ScrollLink>
-                <ScrollLink
-                  to="education"
-                  smooth={true}
-                  duration={50}
-                  className="hover:underline cursor-pointer"
-                >
-                  Education
-                </ScrollLink>
-              </nav>
-            </footer>
-          </nav>
-          <nav>
-            <div className="grid grid-flow-col gap-4">
-              <a href="https://x.com/soniPrince4090" target="_blank" rel="noopener noreferrer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current text-black dark:text-white"
-                >
-                  <path
-                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-                  ></path>
-                </svg>
-              </a>
-              <a href="https://github.com/PrinceSoni4090" target="_blank" rel="noopener noreferrer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current text-black dark:text-white"
-                >
-                  <path
-                    d="M12 .297c-6.63 0-12 5.373-12 12 0 5.302 3.438 9.8 8.205 11.387.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.083-.729.083-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.335-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.3 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.29-1.552 3.297-1.23 3.297-1.23.653 1.653.241 2.873.118 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.803 5.62-5.475 5.92.43.37.823 1.102.823 2.222 0 1.606-.015 2.898-.015 3.293 0 .322.218.694.825.576 4.765-1.588 8.2-6.085 8.2-11.386 0-6.627-5.373-12-12-12z"
-                  ></path>
-                </svg>
-              </a>
-              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current text-black dark:text-white"
-                >
-                  <path
-                    d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.026-3.063-1.867-3.063-1.869 0-2.156 1.459-2.156 2.963v5.704h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.841-1.563 3.039 0 3.6 2.002 3.6 4.604v5.592z"
-                  ></path>
-                </svg>
-              </a>
-            </div>
-          </nav>
-          <aside>
-            <p>Copyright © {new Date().getFullYear()} - All rights reserved.</p>
-          </aside>
-        </footer>
-      </section>
+  <footer className="footer footer-center rounded p-10">
+    <nav className="grid grid-flow-col gap-4">
+      <footer>
+        <nav className=" font-semibold  flex justify-center space-x-4">
+          <ScrollLink
+            to="about"
+            smooth={true}
+            duration={50}
+            className="hover:underline cursor-pointer"
+          >
+            About
+          </ScrollLink>
+          <ScrollLink
+            to="skills"
+            smooth={true}
+            duration={50}
+            className="hover:underline cursor-pointer"
+          >
+            Skills
+          </ScrollLink>
+          <ScrollLink
+            to="projects"
+            smooth={true}
+            duration={50}
+            className="hover:underline cursor-pointer"
+          >
+            Projects
+          </ScrollLink>
+          <ScrollLink
+            to="education"
+            smooth={true}
+            duration={50}
+            className="hover:underline cursor-pointer"
+          >
+            Education
+          </ScrollLink>
+        </nav>
+      </footer>
+    </nav>
+    <nav>
+      <div className="grid grid-flow-col gap-4 items-center justify-center">
+        <a href="https://x.com/soniPrince4090" target="_blank" rel="noopener noreferrer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current text-black dark:text-white"
+          >
+            <path
+              d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+            ></path>
+          </svg>
+        </a>
+        <a href="https://github.com/PrinceSoni4090" target="_blank" rel="noopener noreferrer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current text-black dark:text-white"
+          >
+            <path
+              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.302 3.438 9.8 8.205 11.387.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.083-.729.083-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.335-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.3 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.29-1.552 3.297-1.23 3.297-1.23.653 1.653.241 2.873.118 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.803 5.62-5.475 5.92.43.37.823 1.102.823 2.222 0 1.606-.015 2.898-.015 3.293 0 .322.218.694.825.576 4.765-1.588 8.2-6.085 8.2-11.386 0-6.627-5.373-12-12-12z"
+            ></path>
+          </svg>
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            className="fill-current text-black dark:text-white"
+          >
+            <path
+              d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.026-3.063-1.867-3.063-1.869 0-2.156 1.459-2.156 2.963v5.704h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.841-1.563 3.039 0 3.6 2.002 3.6 4.604v5.592z"
+            ></path>
+          </svg>
+        </a>
+      </div>
+      <div className="font-semibold dark:text-white text-center mt-4">prince4090@gmail.com</div>
+    </nav>
+    <aside>
+      <p>Copyright © {new Date().getFullYear()} - All rights reserved.</p>
+    </aside>
+  </footer>
+</section>
 
     </main>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,6 @@
 'use client'
 import Link from 'next/link';
 import { Button } from './ui/button';
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
 import { ModeToggle } from './theme-btn';
 import LoadingBar from 'react-top-loading-bar';
 import { usePathname } from 'next/navigation';
@@ -42,114 +34,62 @@ const Navbar = () => {
     }, [900])
 
     return (
-        <nav className="p-4 bg-background/50 sticky top-0  backdrop-blur border-b-0 z-10">
-            <LoadingBar
-                color='#933ce6'
-                progress={progress}
-                onLoaderFinished={() => setProgress(0)}
-            />
-            <div className="container mx-auto flex justify-between items-center">
-                <Link href={"/"}>
-                    <div className=" text-lg font-semibold md:mr-2 lg:mr-2 transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg">
-                        Portfolio
-                    </div>
-                </Link>
-                <div className='hidden md:flex space-x-4 items-center'>
-                   <nav className=" font-semibold  flex justify-center space-x-4">
-                        <ScrollLink
-                          to="about"
-                          smooth={true}
-                          duration={50}
-                          className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                        >
-                          About
-                        </ScrollLink>
-                        <ScrollLink
-                          to="skills"
-                          smooth={true}
-                          duration={50}
-                          className="cursor-pointer transition-transform duration=300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                        >
-                          Skills
-                        </ScrollLink>
-                        <ScrollLink
-                          to="projects"
-                          smooth={true}
-                          duration={50}
-                          className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                        >
-                          Projects
-                        </ScrollLink>
-                        <ScrollLink
-                          to="education"
-                          smooth={true}
-                          duration={50}
-                          className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                        >
-                          Education
-                        </ScrollLink>
-                    </nav>
-                    <div className="flex items-center">
-                        <ModeToggle />
-                    </div>
-                </div>
-                 <div className="md:hidden">
-                            <ModeToggle /> </div>
-                {/* <div className="md:hidden">
-                            <ModeToggle /> 
-                   { <Sheet>
-                        <span className="mx-4">
-                        </span>
-                        <SheetTrigger>
-                            <svg className="w-6 h-6 transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                            </svg>
-                        </SheetTrigger>
-                        <SheetContent>
-                            <SheetHeader>
-                                <SheetTitle className="font-bold my-4"> PrinceBlog </SheetTitle>
-                                <SheetDescription>
-                                    <div className="flex flex-col gap-6">
-                                        <ScrollLink
-                                          to="about"
-                                          smooth={true}
-                                          duration={50}
-                                          className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                                        >
-                                          About
-                                        </ScrollLink>
-                                        <ScrollLink
-                                          to="skills"
-                                          smooth={true}
-                                          duration={50}
-                                          className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                                        >
-                                          Skills
-                                        </ScrollLink>
-                                        <ScrollLink
-                                          to="projects"
-                                          smooth={true}
-                                          duration={50}
-                                          className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                                        >
-                                          Projects
-                                        </ScrollLink>
-                                        <ScrollLink
-                                          to="education"
-                                          smooth={true}
-                                          duration={50}
-                                          className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
-                                        >
-                                          Education
-                                        </ScrollLink>
-                                    </div>
-                                </SheetDescription>
-                            </SheetHeader>
-                        </SheetContent>
-                    </Sheet>
-                </div> */}
+       <nav className="p-4 bg-background/50 sticky top-0 backdrop-blur border-b-0 z-10">
+    <LoadingBar
+        color='#933ce6'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+    />
+    <div className="container mx-auto flex justify-between items-center">
+        <Link href={"/"}>
+            <div className="text-lg font-semibold md:mr-2 lg:mr-2 transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg">
+                Portfolio
             </div>
-        </nav>
+        </Link>
+        <div className='hidden md:flex space-x-4 items-center'>
+            <nav className="font-semibold flex justify-center space-x-4">
+                <ScrollLink
+                    to="about"
+                    smooth={true}
+                    duration={50}
+                    className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
+                >
+                    About
+                </ScrollLink>
+                <ScrollLink
+                    to="skills"
+                    smooth={true}
+                    duration={50}
+                    className="cursor-pointer transition-transform duration=300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
+                >
+                    Skills
+                </ScrollLink>
+                <ScrollLink
+                    to="projects"
+                    smooth={true}
+                    duration={50}
+                    className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
+                >
+                    Projects
+                </ScrollLink>
+                <ScrollLink
+                    to="education"
+                    smooth={true}
+                    duration={50}
+                    className="cursor-pointer transition-transform duration-300 hover:text-purple-500 hover:scale-110 hover:shadow-lg"
+                >
+                    Education
+                </ScrollLink>
+            </nav>
+            <div className="flex items-center">
+                <ModeToggle />
+            </div>
+        </div>
+        <div className="md:hidden">
+            <ModeToggle />
+        </div>
+    </div>
+</nav>
     );
 };
 
